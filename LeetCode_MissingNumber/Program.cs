306b9a128c5d5b075637f195af3ac7f162a9a5d4
John Weisz <johnmweisz@gmail.com>
2019-12-13 16:21:56 -0800

    Add project files.

---
+++ LeetCode_MissingNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_MissingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.MissingNumber(new int[] {0, 1, 2, 3, 4, 6}));
+        }
+    }
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            var values = new bool[nums.Length + 1];
+
+            foreach (var n in nums)
+            {
+                values[n] = true;
+            }
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                if (!values[i]) return i;
+            }
+
+            return -1;
+        }
+    }
+}
